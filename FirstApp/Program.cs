c70407fdd544a100ff779736f5a42ff6281e57ba
Balal Gholami <balal.gholami@gmail.com>
2023-07-08 14:34:35 +0330

    First CSharp

---
+++ FirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World...");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Welcome to C#...");
+            Console.ResetColor();
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
